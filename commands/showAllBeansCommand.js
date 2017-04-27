14ebd6c842791e0e8f54f67feeca79b83b4ba4cf
vocrsz <vocrsz@gmail.com>
2017-04-27 16:46:42 +0300

    fix show all beans command

--- commands/showAllBeansCommand.js
+++ commands/showAllBeansCommand.js
@@ -8,31 +8,65 @@ module.exports = class ShowAllBeansCommand {
     return this.handler.bind(this);
   }
   handler() {
-    let allBeansGroupedByPackage = Locate.find('', ['bean']).reduce((groupedByPackage, bean) => {
-      groupedByPackage[bean.package] = groupedByPackage[bean.package] || [];
-      groupedByPackage[bean.package].push({
-        "name": bean.name,
-        "file": bean.file
+    
+    let beansGroupedByPackage = Locate.find('', ['bean']).reduce((groupedByPackage, bean) => {
+      let data = groupedByPackage.find(item => item.name == bean.package);
+      if (data) {
+        data.beans.push({
+          name:          bean.name,
+          url:           bean.file
+        });
+
+        return groupedByPackage;
+      }
+      
+      groupedByPackage.push({ 
+        name:           bean.package, 
+        formattedName:  this.formatPackageToQuickPick(bean.package),
+        url:            PackageParser.getPackageModuleUrl(bean.package),
+        beans: [
+          {
+            name: bean.name,
+            url:  bean.file
+          }
+        ] 
       });
+      
       return groupedByPackage;
-    }, {});
+    }, []);
 
     let quickPickData = [];
-    for (let pkg in allBeansGroupedByPackage) {
-      quickPickData.push(`Package: ${pkg}`);
-      allBeansGroupedByPackage[pkg].forEach(data => quickPickData.push(`      ${data.name}`));
-    }
-
-    vscode.window.showQuickPick(quickPickData, { 
-      placeHolder: 'Do you want to delete duplicate dependency declarations and unused dependencies?'
-    }).then(selectedBean => {
-      if (!selectedBean) return;
-      if (~selectedBean.indexOf('Package:')) return;
-
-      let beanFile = Locate.find(selectedBean.trim(), ['bean'])[0];
-      vscode.workspace.openTextDocument(beanFile.file).then((textDocument) => {
-        vscode.window.showTextDocument(textDocument);
+    beansGroupedByPackage.forEach((pkg, index) => {
+      quickPickData.push(`${++index}. Package: ${pkg.formattedName}`);
+      pkg.beans.forEach((bean, beanIndex) => {
+        quickPickData.push(`    ${index}.${++beanIndex}. ${bean.name}`);
       });
-    })
+    });
+
+    vscode.window.showQuickPick(
+      quickPickData,
+      { placeHolder: "Enter bean name:" }
+    ).then(selectedBean => {
+      if (!selectedBean) return;
+      
+      let packageNumber = +selectedBean.match(/(\d+)\./g)[0] - 1;
+      let beanNumber    = +selectedBean.match(/(\d+)\./g)[1] - 1;
+
+      let dependentFile = isNaN(beanNumber) ? beansGroupedByPackage[packageNumber].url :
+                                              beansGroupedByPackage[packageNumber].beans[beanNumber].url;
+      
+      vscode.workspace.openTextDocument(dependentFile).then((textDocument) => {
+        vscode.window.showTextDocument(textDocument);
+      },
+      (err) => {vscode.window.showErrorMessage(err)});
+    });
+  }
+  formatPackageToQuickPick(pkgName) {
+    return pkgName.split('/')
+                  .slice(-1)
+                  .pop()
+                  .split('_')
+                  .map(s => s.charAt(0).toUpperCase() + s.slice(1) )
+                  .join(' ')
   }
 }
