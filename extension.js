6ca7d231b2f9e066fb11f839f0e932f22ada0b69
vocrsz <vocrsz@gmail.com>
2018-02-13 11:14:59 +0300

    Disable ruby parser

--- extension.js
+++ extension.js
@@ -1,10 +1,10 @@
 const vscode            = require('vscode');
 const VscodeUtils       = require('./utils/vscode_utils');
 
-const provideDefinition = require('./handlers/bean_provider_definition');
+// const provideDefinition = require('./handlers/bean_provider_definition');
 
-const ShowAllBeansCommand  = require('./handlers/show_all_beans_handler');
-const FindBeanUsageHandler = require('./handlers/find_bean_usage_handler');
+// const ShowAllBeansCommand  = require('./handlers/show_all_beans_handler');
+// const FindBeanUsageHandler = require('./handlers/find_bean_usage_handler');
 
 const GoToPackageHandler     = require('./handlers/go_to_package_handler');
 const GeneratePackageHandler = require('./handlers/generate_package_handler');
@@ -19,10 +19,10 @@ function activate(context) {
     if (!vscode.workspace.rootPath) return;
 
     context.subscriptions.push(
-        vscode.languages.registerDefinitionProvider(['ruby'], { provideDefinition: provideDefinition }),
+        // vscode.languages.registerDefinitionProvider(['ruby'], { provideDefinition: provideDefinition }),
 
-        vscode.commands.registerCommand('extension.findBeanUsage',              new FindBeanUsageHandler(VscodeUtils.showQuickPick)),  
-        vscode.commands.registerCommand('extension.showAllBeans',               new ShowAllBeansCommand(VscodeUtils.showQuickPick)),   
+        // vscode.commands.registerCommand('extension.findBeanUsage',              new FindBeanUsageHandler(VscodeUtils.showQuickPick)),  
+        // vscode.commands.registerCommand('extension.showAllBeans',               new ShowAllBeansCommand(VscodeUtils.showQuickPick)),   
         vscode.commands.registerCommand('extension.goToPackage',                new GoToPackageHandler(VscodeUtils.showQuickPick, VscodeUtils.showQuickPick)),    
         vscode.commands.registerCommand('extension.generateNewPackage',         new GeneratePackageHandler(VscodeUtils.showInputBox, VscodeUtils.showQuickPick, VscodeUtils.showInputBox)),
         vscode.commands.registerCommand('extension.generateNewBean',            new GenerateBeanHandler(VscodeUtils.showQuickPick, VscodeUtils.showQuickPick, VscodeUtils.showQuickPick, VscodeUtils.showInputBox)),
